d45d880c780801651a8eea31b282c87cd3ed073c
Nikhil Kollanoor <32312705+nkollanoor@users.noreply.github.com>
2021-01-11 16:26:48 -0500

    Update HighestPageNumber.py

--- HighestPageNumber.py
+++ HighestPageNumber.py
@@ -2,7 +2,7 @@
 input='9910010110299100101103991001011045'
 # actual page numbers = [99100101102,99100101103,99100101104,5]
 ans=[]
-for i in range(1,(len(input)//2)+1): # slot for number of digits
+for i in range((len(input)//2),0,-1): # slot for number of digits
     temp_str=input
     curr=temp_str[:i]
     temp_str=temp_str[i:]
@@ -13,5 +13,6 @@ for i in range(1,(len(input)//2)+1): # slot for number of digits
         temp_str=temp_str[len(str(int(curr)+1)):]
         f=1
     if f==1: # if at least one successive page is found, append the result.
-        ans.append([i,curr])
-print(ans[-1][-1])  # ans = [[2, '102'], [11, '99100101104']] -  gives starting length of page number with highest page number associated
+        print(curr)
+        break
+ # ans = [[2, '102'], [11, '99100101104']] -  gives starting length of page number with highest page number associated
